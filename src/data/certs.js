c65c2e1b700809bb10145a47791f1ab86bbf5955
Bug Fix <tf.khoindvn@gmail.com>
2026-05-31 02:33:49 +0700

    Add files via upload

---
+++ src/data/certs.js
@@ -0,0 +1,303 @@
+// Dữ liệu chứng chỉ ESign/KSign — tách riêng khỏi giao diện.
+// Sửa danh sách ở đây để thêm/bớt chứng chỉ; không cần đụng tới component.
+
+export const certs = {
+    ksign: [
+      {
+        name: "KSign",
+        url: "https://api.khoindvn.io.vn/ujCv4c",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "Beijing Xibei Longzhimeng Restaurant Management Co., Ltd."
+      },
+      {
+        name: "KSign",
+        url: "https://api.khoindvn.io.vn/qzCfvO",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "BOC International (China) Limited"
+      },
+      {
+        name: "KSign BOC V1",
+        url: "https://api.khoindvn.io.vn/lkJx5H",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "BOC International (China) Limited"
+      },
+      {
+        name: "KSign BOC V2",
+        url: "https://api.khoindvn.io.vn/vNBrUk",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "BOC International (China) Limited"
+      },
+      {
+        name: "KSign BOC V3",
+        url: "https://api.khoindvn.io.vn/vZDgEf",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "BOC International (China) Limited"
+      },
+      {
+        name: "KSign",
+        url: "https://api.khoindvn.io.vn/glqjrT",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "Aramco Services Company"
+      },
+      {
+        name: "KSign",
+        url: "https://api.khoindvn.io.vn/PmIJKe",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "Beijing Zhizhangyi Inc."
+      },
+      {
+        name: "KSign",
+        url: "https://api.khoindvn.io.vn/NiwZ04",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "National Oilwell Varco, Inc."
+      },
+      {
+        name: "KSign",
+        url: "https://api.khoindvn.io.vn/Pm9ZYC",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "Qingdao Rural Commercial Bank Co., Ltd"
+      },
+      {
+        name: "KSign",
+        url: "https://api.khoindvn.io.vn/dSCrTo",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "Commission on Elections"
+      },
+      {
+        name: "KSign",
+        url: "https://api.khoindvn.io.vn/wEPzVr",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "Luoyang Postal Administration"
+      },
+      {
+        name: "KSign",
+        url: "https://api.khoindvn.io.vn/ri26yM",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "PowerChina International Group Limited"
+      },
+      {
+        name: "KSign PowerChina V1",
+        url: "https://api.khoindvn.io.vn/FNzQhU",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "PowerChina International Group Limited"
+      },
+      {
+        name: "KSign PowerChina V2",
+        url: "https://api.khoindvn.io.vn/bsLHVL",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "PowerChina International Group Limited"
+      },
+      {
+        name: "KSign PowerChina V3",
+        url: "https://api.khoindvn.io.vn/XdxwPt",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "PowerChina International Group Limited"
+      },
+      {
+        name: "KSign PowerChina V4",
+        url: "https://api.khoindvn.io.vn/k28bBo",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "PowerChina International Group Limited"
+      },
+      {
+        name: "KSign",
+        url: "https://api.khoindvn.io.vn/JqwGx1",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "GLOBAL TAKEOFF, INC"
+      },
+      {
+        name: "KSign GLOBAL V1",
+        url: "https://api.khoindvn.io.vn/0VzJGH",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "GLOBAL TAKEOFF, INC"
+      },
+      {
+        name: "KSign GLOBAL V2",
+        url: "https://api.khoindvn.io.vn/QoniLw",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "GLOBAL TAKEOFF, INC"
+      },
+      {
+        name: "KSign GLOBAL V3",
+        url: "https://api.khoindvn.io.vn/mufnGR",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "GLOBAL TAKEOFF, INC"
+      },
+      {
+        name: "KSign GLOBAL V4",
+        url: "https://api.khoindvn.io.vn/sXQUUV",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "GLOBAL TAKEOFF, INC"
+      },
+      {
+        name: "KSign GLOBAL V5",
+        url: "https://api.khoindvn.io.vn/X41tJA",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "GLOBAL TAKEOFF, INC"
+      },
+      {
+        name: "KSign GLOBAL V6",
+        url: "https://api.khoindvn.io.vn/uEHxf0",
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYlk6oHRX_isIEsA0183yypCcVuMNaseuGzSymQIUY1g&s',
+        descriptions: "GLOBAL TAKEOFF, INC"
+      }
+    ],
+    esign: [
+      {
+        name: "ESign",
+        url: "https://api.khoindvn.io.vn/i4dHNr",
+        img: 'img/pf/profile-pic.png',
+        descriptions: "Beijing Xibei Longzhimeng Restaurant Management Co., Ltd."
+      },
+      {
+        name: 'ESign',
+        url: 'https://api.khoindvn.io.vn/nbSQ2q',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'BOC International (China) Limited'
+      },
+      {
+        name: 'ESign BOC V1',
+        url: 'https://api.khoindvn.io.vn/Fv5vFZ',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'BOC International (China) Limited'
+      },
+      {
+        name: 'ESign BOC V2',
+        url: 'https://api.khoindvn.io.vn/FLwKeO',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'BOC International (China) Limited'
+      },
+      {
+        name: 'ESign BOC V3',
+        url: 'https://api.khoindvn.io.vn/Lxiubx',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'BOC International (China) Limited'
+      },
+      {
+        name: 'ESign',
+        url: 'https://api.khoindvn.io.vn/eQDb5c',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Aramco Services Company'
+      },
+      {
+        name: 'ESign',
+        url: 'https://api.khoindvn.io.vn/jCh80j',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Beijing Zhizhangyi Inc.'
+      },
+      {
+        name: 'ESign',
+        url: 'https://api.khoindvn.io.vn/SiRDpZ',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'National Oilwell Varco, Inc.'
+      },
+      {
+        name: 'ESign',
+        url: 'https://api.khoindvn.io.vn/IosVVa',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'VIETNAM AIRLINES JSC'
+      },
+      {
+        name: 'ESign',
+        url: 'https://api.khoindvn.io.vn/JHqEjv',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Qingdao Rural Commercial Bank Co., Ltd'
+      },
+      {
+        name: 'ESign',
+        url: 'https://api.khoindvn.io.vn/CkOyq3',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Commission on Elections'
+      },
+      {
+        name: 'ESign Com V1',
+        url: 'https://api.khoindvn.io.vn/6UMBaC',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Commission on Elections'
+      },
+      {
+        name: 'ESign Com V2',
+        url: 'https://api.khoindvn.io.vn/GX5cq7',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Commission on Elections'
+      },
+      {
+        name: 'ESign',
+        url: 'https://api.khoindvn.io.vn/m3iXgW',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Luoyang Postal Administration'
+      },
+      {
+        name: 'ESign',
+        url: 'https://api.khoindvn.io.vn/VlLV6l',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'PowerChina International Group Limited'
+      },
+      {
+        name: 'ESign PowerChina V1',
+        url: 'https://api.khoindvn.io.vn/7944EP',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'PowerChina International Group Limited'
+      },
+      {
+        name: 'ESign PowerChina V2',
+        url: 'https://api.khoindvn.io.vn/btX260',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'PowerChina International Group Limited'
+      },
+      {
+        name: 'ESign PowerChina V3',
+        url: 'https://api.khoindvn.io.vn/cDNdxx',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'PowerChina International Group Limited'
+      },
+      {
+        name: 'ESign PowerChina V4',
+        url: 'https://api.khoindvn.io.vn/mobNoQ',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'PowerChina International Group Limited'
+      },
+      {
+        name: 'ESign',
+        url: 'https://api.khoindvn.io.vn/4D8wjF',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'
+      },
+      {
+        name: 'ESign GlOBAL V1',
+        url: 'https://api.khoindvn.io.vn/hZgAt7',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'
+      },
+      {
+        name: 'ESign GlOBAL V2',
+        url: 'https://api.khoindvn.io.vn/fnOXAo',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'
+      },
+      {
+        name: 'ESign GlOBAL V3',
+        url: 'https://api.khoindvn.io.vn/BEZcKD',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'
+      },
+      {
+        name: 'ESign GlOBAL V4',
+        url: 'https://api.khoindvn.io.vn/skjKPF',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'
+      },
+      {
+        name: 'ESign GlOBAL V5',
+        url: 'https://api.khoindvn.io.vn/VjHmnf',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'
+      },
+      {
+        name: 'ESign GlOBAL V6',
+        url: 'https://api.khoindvn.io.vn/ufbyRf',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'
+      }
+    ]
+  };
